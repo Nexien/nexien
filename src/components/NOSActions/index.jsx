6dc850a66f9a5e50d22d5ac1b9bd8ae6cdf52eae
matprime <matt@mattprimeonline.com>
2018-06-05 10:48:37 -0400

    changes to form control

--- src/components/NOSActions/index.jsx
+++ src/components/NOSActions/index.jsx
@@ -13,23 +13,13 @@ const styles = {
 };
 
 class NOSActions extends React.Component {
-
-  handleSubmitGetAddress(e){
-    alert("You have clicked button: Get Address");
-    e.preventDefault();
-  }
-
-  handleExchangePreview(e){
-    alert("You have clicked button: Excange preview");
-    e.preventDefault();
-  }
-
-  handleSendTokens(e){
-    alert("You have clicked button: SEND TOKENS");
-    e.preventDefault();
-  }
+  handleSubmitGetAddress = () => alert("You have clicked button: Get Address");
 
   handleGetAddress = async () => alert(await this.props.nos.getAddress());
+  handleExchangePreview = () =>
+    alert("You have clicked button: Exchange preview");
+
+  handleSendTokens = () => alert("You have clicked button: SEND TOKENS");
 
   render() {
     const { classes } = this.props;
@@ -39,23 +29,35 @@ class NOSActions extends React.Component {
         <h3>Define Exchange Transaction</h3>
         <form className={classes.form} onSubmit={this.handleSubmitGetAddress}>
           <div>
-            <label>Enter HASH of transaction, <br /> which includes NEP5 tokens to exchange </label>
-            <input type="text" ref="transaction" />
-            <input type="submit" value="Get Address" />
-          </div>
-        </form> <br />
-        <h3>YOU WILL SEND</h3>
-        <form className={classes.form} onSubmit={this.handleExchangePreview}>
-          <div>
-            <label>Select type of NEP5 token to send </label>
-            <input type="text" ref="send" /> <br />
-            <label>Enter value of NEP5 tokens </label>
-            <input type="text" ref="value" /> <br />
-            <label>Select type of NEP5 token to get </label>
-            <input type="text" ref="value" /> <br />
-            <input type="submit" value="Exchange Transaction Preview" />
+            <label htmlFor="transaction">
+              Enter HASH of transaction, <br /> which includes NEP5 tokens to
+              exchange
+            </label>
+            <input id="transction" type="text" />
+            <input type="submit" value="Get Transaction" />
           </div>
         </form>
+        <br />
+        <h3>YOU WILL SEND</h3>
+        <form className={classes.form} onSubmit={this.handleExchangePreview}>
+          <label htmlFor="tokenSend">
+            Select type of NEP5 token to send
+            <input id="tokenSend" type="text" />
+          </label>
+          <br />
+          <label htmlFor="tokenValue">
+            Enter value of NEP5 tokens to send
+            <input id="tokenValue" type="text" />
+          </label>
+          <br />
+          <label htmlFor="tokenGet">
+            Select type of NEP5 token to get
+            <input id="tokenGet" type="text" />
+          </label>
+          <br />
+          <input type="submit" value="Exchange Transaction Preview" />
+        </form>
+        <br />
         <h3>YOU WILL GET</h3>
         <button className={classes.button} onClick={this.handleSendTokens}>
           SEND TOKENS
