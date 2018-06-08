3c4b3e793919c78b371a7d962a388a13a079e86d
matprime <matt@mattprimeonline.com>
2018-06-08 05:32:17 -0400

    changed dApp logo and animation properties

--- src/components/SpinningLogo/index.jsx
+++ src/components/SpinningLogo/index.jsx
@@ -2,33 +2,33 @@ import React from "react";
 import injectSheet from "react-jss";
 import PropTypes from "prop-types";
 
-import nosLogo from "./../../assets/logo.png";
+import nexienLogo from "./../../assets/Nexien.png";
 
 const styles = {
   logoContainer: {
-    //  display: "block",
-    display: "none",
+    display: "block",
+    // display: "none",
     maxWidth: "200px",
     margin: "32px auto",
     position: "relative"
   },
-  nosLogo: {
-    animation: "nOS-logo-spin infinite 3s linear",
+  nexienLogo: {
+    animation: "nOS-logo-spin 3s linear",
     width: "100%"
   },
   "@keyframes nOS-logo-spin": {
     from: { transform: "rotate(0deg)" },
-    to: { transform: "rotate(360deg)" }
+    to: { transform: "rotate(180deg)" }
   }
 };
 
 const SpinningLogo = ({ classes }) => (
   <div className={classes.logoContainer}>
     <img
-      src={nosLogo}
-      className={classes.nosLogo}
-      alt="nOS logo"
-      key="nosLogo"
+      src={nexienLogo}
+      className={classes.nexienLogo}
+      alt="Nexien logo"
+      key="nexienLogo"
     />
   </div>
 );
